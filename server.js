4afaa8fb99571d64ff06959b50802ae64ec6758c
Atul Sinha <itsatul0@gmail.com>
2017-02-05 16:55:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/itsatul0.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'itsatul0.jpg'));
 });
